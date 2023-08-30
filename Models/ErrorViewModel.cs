5c8ad430164fd2c7d9fead07e1ec2682b20d622f
Saikoo-Stark <alwkylsyd87@gmail.com>
2023-08-30 20:31:42 +0300

    creating project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace my_Ecommerce_App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
